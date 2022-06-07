74e4c2f4701d03a28353151dbea0c8bacd3c6eae
rin2-5 <linjiax25@gmail.com>
2022-06-07 12:19:48 +0800

    2nd commit

--- App.js
+++ App.js
@@ -1,9 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar, TouchableOpacity } from "react-native-web"
+import { View, Text, StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import BlockRGB from "./components/BlockRGB";
+import { FlatList } from "react-native-gesture-handler";
+
+function HomeScreen() {
+  const [colorArray, setColorArray] = useState([
+    {red: 255, green: 0, blue: 0, id: "0"},
+    {red: 0, green: 255, blue: 0, id: "1"},
+    {red: 0, green: 0, blue: 255, id: "2"},
+  ])
+
+  function renderItem({item}) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} key={item.id} />
+  }
+
+  function addColor() {
+    setColorArray([
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+      }
+    ])
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+
+      </TouchableOpacity>
+      <FlatList>
+      style={styles.list}
+      data={colorArray}
+      renderItem={renderItem}
+      </FlatList>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
